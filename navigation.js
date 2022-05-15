a9f6784d31108d9a25da666e58d757a38f3b7501
Sabrina Grivas <sgrivas1@gmail.com>
2022-05-15 17:28:41 -0400

    navigation styling

--- navigation.js
+++ navigation.js
@@ -1,14 +1,4 @@
-function showMenu(){
-    document.querySelector(".navbar").classList.toggle("open");
-    document.querySelector(".nav-list").classList.toggle("open");
-}
-
-/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
-function myFunction() {
-  var x = document.getElementById("myTopnav");
-  if (x.className === "topnav") {
-    x.className += " responsive";
-  } else {
-    x.className = "topnav";
-  }
+function showMenu() {
+  document.querySelector(".navbar").classList.toggle("open");
+  document.querySelector(".nav-list").classList.toggle("open");
 }
